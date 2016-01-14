56cc2aff5b0d7cc16095a1ff1e89b995f91b9a68
Peter Solow <petersolow@gmail.com>
2016-01-14 16:19:06 +0100

    initial

---
+++ Interface/resources/js/modules/foundation-loader.js
@@ -0,0 +1,39 @@
+/*global define:false, Modernizr:false */
+
+define([
+    'jquery',
+    'foundation.core',
+    'foundation.util.box',
+    'foundation.util.keyboard',
+    'foundation.util.mediaQuery',
+    'foundation.util.motion',
+    'foundation.util.nest',
+    'foundation.util.timerAndImageLoader',
+    'foundation.util.touch',
+    'foundation.util.triggers',
+    'foundation.accordion',
+    'foundation.accordionMenu',
+    'foundation.drilldown',
+    'foundation.orbit',
+    'foundation.responsiveMenu',
+    'foundation.responsiveToggle',
+    'foundation.reveal',
+    'foundation.slider',
+    'foundation.sticky',
+    'foundation.tabs',
+    'foundation.toggler',
+    'foundation.tooltip',
+    'foundation.abide',
+    'foundation.equalizer',
+    'foundation.magellan',
+    'foundation.offcanvas',
+    'foundation.dropdown',
+    'foundation.dropdownMenu',
+    'foundation.interchange'
+    
+    
+], function ($) {
+    'use strict';
+    // bootstrap foundation
+    $(document).foundation();
+});
---
+++ Interface/resources/js/modules/module-example.js
@@ -0,0 +1,6 @@
+/*global define:false, Modernizr:false */
+define(['jquery'], function ($) {
+    'use strict';
+
+    
+});
