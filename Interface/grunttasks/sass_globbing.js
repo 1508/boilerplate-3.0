2dbef88c075d3a31588a11dd9704b601cbbe5162
Sune Radich Vestergaard <srv@1508.dk>
2016-11-18 10:55:34 +0100

    Use property for interface root in package.json

--- Interface/grunttasks/sass_globbing.js
+++ Interface/grunttasks/sass_globbing.js
@@ -1,11 +1,11 @@
-module.exports = {
+omodule.exports = {
     options: {
 
     },
     // Generate a main stylesheet for the Umbraco Styling Plugin, to import all .umbraco.scss files
     umbracoStyling: {
         files: {
-            'Interface/umbraco/css/styles.scss': ['<%= package.resources %>/sass/**/*.umbraco.scss']
+            '<%= package.interfaceRoot %>/umbraco/css/styles.scss': ['<%= package.resources %>/sass/**/*.umbraco.scss']
         }
     }
 
