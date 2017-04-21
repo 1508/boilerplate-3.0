3f5bd3fb13c617f6c924a99c6ebcf8ef161eb766
Sune Radich Vestergaard <srv@1508.dk>
2017-04-21 09:32:28 +0200

    Replace grunt-express with grunt-contrib-connect
    The grunt-express plugin is no longer being maintained, and used a peer-dependency to an old version of grunt.

---
+++ Interface/grunttasks/connect.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    prototype: {
+        options: {
+            port: 1508,
+            protocol: 'http',
+            livereload: true
+            base: ['<%= package.prototype %>'],
+            open: 'http://localhost:1508'
+        }
+    }
+
+}
--- Interface/grunttasks/open.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    prototype: {
-        path: 'http://localhost:1508'
-    }
-};
